009d7d980188bd0bc80833f3608456ef48daf422
Elton Silva <eltoncarsilva73@gmail.com>
2021-05-21 16:32:46 -0300

    user login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+    def show
+        #@user = User.find_by(id: params[:id])
+        #if(@user.present?)
+        #    render :view
+        #else
+        #    redirect_to new_user_session_path
+        #end
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, skip: [:sessions, :registrations, :passwords]
+
+  devise_scope :user do
+
+    get    'login',  to: 'devise/sessions#new',     as: :new_user_session
+    post   'login',  to: 'devise/sessions#create',  as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    put    '/account',  to: 'devise/registrations#update'
+    delete '/account',  to: 'devise/registrations#destroy'
+    post   '/account',  to: 'devise/registrations#create'
+    get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
+    get    '/account',  to: 'devise/registrations#edit',   as: :edit_user_registration
+    patch  '/account',  to: 'devise/registrations#update', as: :user_registration
+    get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+
+    get   'new-pass',  to: 'devise/passwords#new',    as: :new_user_password
+    get   'edit-pass', to: 'devise/passwords#edit',   as: :edit_user_password
+    patch 'edit-pass', to: 'devise/passwords#update', as: :patch_user_password
+    post  'new-pass',  to: 'devise/passwords#create', as: :user_password
+  end
+  #devise_for :sessions
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
